12988a7b64306d01f1f7f646b9ef26d098d1515a
Luaan <luaan@deuterosx.org>
2010-04-02 18:21:22 +0000

    Added a helper static class for common Tao OpenGl calls. Added a method SetColor wrapping Gl.glColor4f.

---
+++ DeuterosX/Teraluwide.Blackbird.Core/GlHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using Tao.OpenGl;
+
+namespace Teraluwide.Blackbird.Core
+{
+	public static class GlHelper
+	{
+		/// <summary>
+		/// Sets the color.
+		/// </summary>
+		/// <param name="color">The color.</param>
+		public static void SetColor(Color color)
+		{
+			Gl.glColor4f((float)color.R / 255f, (float)color.G / 255f, (float)color.B / 255f, (float)color.A / 255f);
+		}
+	}
+}
